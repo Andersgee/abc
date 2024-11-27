10ac33674dc6fc24a7b2a32d4f19f18da481bb38
andy <andersgee@gmail.com>
2024-11-27 21:04:01 +0100

    I mean lets try to tie it together with indexedDB now I guess

--- src/lib/trpc/client/provider.tsx
+++ src/lib/trpc/client/provider.tsx
@@ -1,46 +1,42 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 //import { httpBatchLink } from "@trpc/client";
-import { useState } from "react";
 import { trpc } from "./trpc";
 import { customLink } from "../custom-link";
 import { idbLink } from "../idb-link";
 //import { trpc } from './utils/trpc';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      enabled: true,
+      staleTime: Infinity,
+      refetchInterval: false,
+      refetchIntervalInBackground: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      retryOnMount: true,
+      throwOnError: false,
+      retry: false,
+      //retry: 3,
+      //retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+      //the default JSON.sringify will error on anything without .toJSON() method. like bigint or typed arrays etc
+      //queryKeyHashFn: (k) => JSONE.stringify(k),
+    },
+  },
+});
+
+const trpcClient = trpc.createClient({
+  links: [
+    customLink,
+    idbLink(),
+    //httpBatchLink({
+    //  url: "http://localhost:3000/trpc",
+    //}),
+  ],
+});
+
 export function TrpcProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            enabled: true,
-            staleTime: Infinity,
-            refetchInterval: false,
-            refetchIntervalInBackground: false,
-            refetchOnWindowFocus: false,
-            refetchOnReconnect: false,
-            refetchOnMount: false,
-            retryOnMount: true,
-            throwOnError: false,
-            retry: false,
-            //retry: 3,
-            //retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
-            //the default JSON.sringify will error on anything without .toJSON() method. like bigint or typed arrays etc
-            //queryKeyHashFn: (k) => JSONE.stringify(k),
-          },
-        },
-      })
-  );
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        customLink,
-        idbLink(),
-        //httpBatchLink({
-        //  url: "http://localhost:3000/trpc",
-        //}),
-      ],
-    })
-  );
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
