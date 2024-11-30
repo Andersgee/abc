ca1a5e6574144cbde3223d0de2943c7afb0ad634
andy <andersgee@gmail.com>
2024-11-30 20:43:07 +0100

    all basic crud routes

--- src/table4.tsx
+++ src/table4.tsx
@@ -8,62 +8,26 @@ type Props = {
 };
 
 export function Table4({ className }: Props) {
-  const utils = api.useUtils();
   const { data, isLoading } = api.table4.getAll.useQuery();
-  const { data: data2, isLoading: isLoading2 } = api.table4.get.useQuery({
-    id: 2,
-  });
-
-  const { mutate: add, isPending } = api.table4.add.useMutation({
-    onSuccess(data) {
-      utils.table4.invalidate();
-      console.log("add, onSuccess, data:", data);
-    },
-    onError(error) {
-      console.log("add, onError, error:", error);
-    },
-  });
-
-  const { mutate: clear } = api.table4.clear.useMutation({
-    onSuccess(data) {
-      utils.table4.invalidate();
-      console.log("clear, onSuccess, data:", data);
-    },
-    onError(error) {
-      console.log("clear, onError, error:", error);
-    },
-  });
 
   return (
     <div className={cn("", className)}>
       <div>isLoading {isLoading ? "yep" : "nope"}</div>
       {data?.map((row) => (
         <div key={row.id} className="flex">
-          <Input row={row} />
-          <Delete row={row} />
+          <InputPut row={row} />
+          <ButtonDelete row={row} />
         </div>
       ))}
-      <div>data: {JSONE.stringify(data)}</div>
+      <InputAdd />
 
-      <div>isLoading2 {isLoading2 ? "yep" : "nope"}</div>
-      <div>data2: {JSONE.stringify(data2)}</div>
-
-      <button
-        className="p-3 bg-green-500"
-        onClick={() => add({ hello: "from onclick" })}
-      >
-        ADD
-      </button>
-      <div>isPending {isPending ? "yep" : "nope"}</div>
-
-      <button className="p-3 bg-red-400" onClick={() => clear()}>
-        CLEAR
-      </button>
+      <ButtonClear />
+      <pre>data: {JSONE.stringify(data, 2)}</pre>
     </div>
   );
 }
 
-function Input({ row }: { row: { id: number; hello: string } }) {
+function InputPut({ row }: { row: { id: number; hello: string } }) {
   const ref = useRef<HTMLInputElement>(null);
 
   const utils = api.useUtils();
@@ -96,7 +60,42 @@ function Input({ row }: { row: { id: number; hello: string } }) {
   );
 }
 
-function Delete({ row }: { row: { id: number; hello: string } }) {
+function InputAdd() {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const utils = api.useUtils();
+  const { mutate } = api.table4.add.useMutation({
+    onSuccess(data) {
+      utils.table4.invalidate();
+      console.log("add, onSuccess, data:", data);
+    },
+    onError(error) {
+      console.log("add, onError, error:", error);
+    },
+  });
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        if (ref.current?.value) {
+          mutate({ hello: ref.current.value });
+          ref.current.value = "";
+        }
+      }}
+    >
+      <input
+        ref={ref}
+        className="p-3 bg-neutral-200"
+        defaultValue=""
+        placeholder="add"
+      ></input>
+    </form>
+  );
+}
+
+function ButtonDelete({ row }: { row: { id: number; hello: string } }) {
   const utils = api.useUtils();
   const { mutate } = api.table4.delete.useMutation({
     onSuccess(data) {
@@ -116,6 +115,30 @@ function Delete({ row }: { row: { id: number; hello: string } }) {
       }}
     >
       delete
+    </button>
+  );
+}
+
+function ButtonClear() {
+  const utils = api.useUtils();
+  const { mutate } = api.table4.clear.useMutation({
+    onSuccess(data) {
+      utils.table4.invalidate();
+      console.log("clear, onSuccess, data:", data);
+    },
+    onError(error) {
+      console.log("clear, onError, error:", error);
+    },
+  });
+
+  return (
+    <button
+      className="p-2 bg-red-800 block"
+      onClick={() => {
+        mutate();
+      }}
+    >
+      CLEAR ALL
     </button>
   );
 }
