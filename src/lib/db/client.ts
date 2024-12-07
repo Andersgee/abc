e76a98b4e69e60f58c3c203f07027863a8f4e76e
andy <andersgee@gmail.com>
2024-12-07 15:59:50 +0100

    enforce tables have key prop

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -1,149 +1,152 @@
-import type { DB } from "./schema";
+//import type { DB } from "./schema";
 
 export function db() {
   if (!window.db) throw new Error("no window.db");
   return window.db;
 }
 
-function read<T extends keyof DB>(tableName: T) {
-  return db().transaction(tableName, "readonly").objectStore(tableName);
-}
+export class Idb<DB extends Record<string, { key: IDBValidKey }>> {
+  constructor() {
+    //init
+  }
+  read<T extends keyof DB & string>(tableName: T) {
+    return db().transaction(tableName, "readonly").objectStore(tableName);
+  }
+  write<T extends keyof DB & string>(tableName: T) {
+    return db().transaction(tableName, "readwrite").objectStore(tableName);
+  }
 
-function write<T extends keyof DB>(tableName: T) {
-  return db().transaction(tableName, "readwrite").objectStore(tableName);
-}
+  // IDBObjectStore instance methods
+  // https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore
 
-export async function dbget<T extends keyof DB>(
-  tableName: T,
-  query: IDBValidKey | IDBKeyRange
-) {
-  return new Promise<DB[T] | null>((resolve, reject) => {
-    const req = read(tableName).get(query);
-    req.onerror = () => reject();
-    req.onsuccess = (e) => resolve((e.target as IDBRequest).result ?? null);
-  });
-}
+  get<T extends keyof DB & string>(
+    tableName: T,
+    query: IDBValidKey | IDBKeyRange
+  ) {
+    return new Promise<DB[T] | null>((resolve, reject) => {
+      const req = this.read(tableName).get(query);
+      req.onerror = () => reject();
+      req.onsuccess = (e) => resolve((e.target as IDBRequest).result ?? null);
+    });
+  }
 
-export async function dbgetKey<T extends keyof DB>(
-  tableName: T,
-  query: IDBValidKey | IDBKeyRange
-) {
-  return new Promise<DB[T]["key"] | null>((resolve, reject) => {
-    const req = read(tableName).getKey(query);
-    req.onerror = () => reject();
-    req.onsuccess = (e) => resolve((e.target as IDBRequest).result ?? null);
-  });
-}
+  getKey<T extends keyof DB & string>(
+    tableName: T,
+    query: IDBValidKey | IDBKeyRange
+  ) {
+    return new Promise<DB[T]["key"] | null>((resolve, reject) => {
+      const req = this.read(tableName).getKey(query);
+      req.onerror = () => reject();
+      req.onsuccess = (e) => resolve((e.target as IDBRequest).result ?? null);
+    });
+  }
 
-export async function dbgetAll<T extends keyof DB>(
-  tableName: T,
-  query?: IDBValidKey | IDBKeyRange | null,
-  count?: number
-) {
-  return new Promise<Array<DB[T]>>((resolve, reject) => {
-    const req = read(tableName).getAll(query, count);
-    req.onerror = () => reject();
-    req.onsuccess = (e) => resolve((e.target as IDBRequest).result);
-  });
-}
+  getAll<T extends keyof DB & string>(
+    tableName: T,
+    query?: IDBValidKey | IDBKeyRange | null,
+    count?: number
+  ) {
+    return new Promise<Array<DB[T]>>((resolve, reject) => {
+      const req = this.read(tableName).getAll(query, count);
+      req.onerror = () => reject();
+      req.onsuccess = (e) => resolve((e.target as IDBRequest).result);
+    });
+  }
 
-export async function dbgetAllKeys<T extends keyof DB>(
-  tableName: T,
-  query?: IDBValidKey | IDBKeyRange | null,
-  count?: number
-) {
-  return new Promise<Array<DB[T]["key"]>>((resolve, reject) => {
-    const req = read(tableName).getAllKeys(query, count);
-    req.onerror = () => reject();
-    req.onsuccess = (e) => resolve((e.target as IDBRequest).result);
-  });
-}
+  getAllKeys<T extends keyof DB & string>(
+    tableName: T,
+    query?: IDBValidKey | IDBKeyRange | null,
+    count?: number
+  ) {
+    return new Promise<Array<DB[T]["key"]>>((resolve, reject) => {
+      const req = this.read(tableName).getAllKeys(query, count);
+      req.onerror = () => reject();
+      req.onsuccess = (e) => resolve((e.target as IDBRequest).result);
+    });
+  }
 
-export async function dbcount<T extends keyof DB>(tableName: T) {
-  return new Promise<number>((resolve, reject) => {
-    const req = read(tableName).count();
-    req.onerror = () => reject();
-    req.onsuccess = (e) => resolve((e.target as IDBRequest).result);
-  });
-}
+  count<T extends keyof DB & string>(tableName: T) {
+    return new Promise<number>((resolve, reject) => {
+      const req = this.read(tableName).count();
+      req.onerror = () => reject();
+      req.onsuccess = (e) => resolve((e.target as IDBRequest).result);
+    });
+  }
 
-export async function dbadd<T extends keyof DB, V extends DB[T]>(
-  tableName: T,
-  value: V
-) {
-  return new Promise<DB[T]["key"]>((resolve, reject) => {
-    const req = write(tableName).add(value);
-    req.onerror = () => reject();
-    req.onsuccess = (event) => resolve((event.target as IDBRequest).result);
-  });
-}
+  add<T extends keyof DB & string, V extends DB[T]>(tableName: T, value: V) {
+    return new Promise<DB[T]["key"]>((resolve, reject) => {
+      const req = this.write(tableName).add(value);
+      req.onerror = () => reject();
+      req.onsuccess = (event) => resolve((event.target as IDBRequest).result);
+    });
+  }
 
-export async function dbclear<T extends keyof DB>(tableName: T) {
-  return new Promise<void>((resolve, reject) => {
-    const req = write(tableName).clear();
-    req.onerror = () => reject();
-    req.onsuccess = () => resolve();
-  });
-}
+  clear<T extends keyof DB & string>(tableName: T) {
+    return new Promise<void>((resolve, reject) => {
+      const req = this.write(tableName).clear();
+      req.onerror = () => reject();
+      req.onsuccess = () => resolve();
+    });
+  }
 
-export async function dbput<T extends keyof DB, V extends DB[T]>(
-  tableName: T,
-  value: V
-) {
-  return new Promise<DB[T]["key"]>((resolve, reject) => {
-    const req = write(tableName).put(value);
-    req.onerror = () => reject();
-    req.onsuccess = (event) => resolve((event.target as IDBRequest).result);
-  });
-}
+  put<T extends keyof DB & string, V extends DB[T]>(tableName: T, value: V) {
+    return new Promise<DB[T]["key"]>((resolve, reject) => {
+      const req = this.write(tableName).put(value);
+      req.onerror = () => reject();
+      req.onsuccess = (event) => resolve((event.target as IDBRequest).result);
+    });
+  }
 
-export async function dbdelete<T extends keyof DB>(
-  tableName: T,
-  query: IDBValidKey | IDBKeyRange
-) {
-  return new Promise<void>((resolve, reject) => {
-    const req = write(tableName).delete(query);
-    req.onerror = () => reject();
-    req.onsuccess = () => resolve();
-  });
-}
-/*
-export async function dbopenCursor<T extends keyof DB>(
-  tableName: T,
-  query?: IDBValidKey | IDBKeyRange | null,
-  direction?: IDBCursorDirection
-) {
-  return new Promise<IDBCursorWithValue>((resolve, reject) => {
-    const req = read(tableName).openCursor(query, direction);
-    req.onerror = () => reject();
-    req.onsuccess = (event) => {
-      const cursor: IDBCursorWithValue = (event.target as IDBRequest).result;
-      resolve(cursor);
-    };
-  });
-}
-*/
-export async function dbopenCursorCallback<T extends keyof DB>(
-  tableName: T,
-  cb: (value: DB[T]) => void,
-  query?: Query,
-  direction?: IDBCursorDirection
-) {
-  return new Promise<void>((resolve, reject) => {
-    const req = read(tableName).openCursor(keyRange(query), direction);
-    req.onerror = () => reject();
-    req.onsuccess = (event) => {
-      const cursor: IDBCursorWithValue = (event.target as IDBRequest).result;
+  delete<T extends keyof DB & string>(
+    tableName: T,
+    query: IDBValidKey | IDBKeyRange
+  ) {
+    return new Promise<void>((resolve, reject) => {
+      const req = this.write(tableName).delete(query);
+      req.onerror = () => reject();
+      req.onsuccess = () => resolve();
+    });
+  }
 
-      if (cursor) {
-        cb(cursor.value);
-        cursor.continue();
-      } else {
-        // no more results
-        resolve();
+  ///////////////
+  // utilities //
+  ///////////////
+
+  openCursorCallback<T extends keyof DB & string>(
+    tableName: T,
+    cb: (value: DB[T]) => void,
+    query?: Query,
+    direction?: IDBCursorDirection
+  ) {
+    return new Promise<void>((resolve, reject) => {
+      const req = this.read(tableName).openCursor(keyRange(query), direction);
+      req.onerror = () => reject();
+      req.onsuccess = (event) => {
+        const cursor: IDBCursorWithValue = (event.target as IDBRequest).result;
+
+        if (cursor) {
+          cb(cursor.value);
+          cursor.continue();
+        } else {
+          // no more results
+          resolve();
+        }
+      };
+    });
+  }
+
+  async filter<T extends keyof DB & string>(
+    tableName: T,
+    predicate: (value: DB[T]) => boolean
+  ) {
+    const rows: Array<DB[T]> = [];
+    await this.openCursorCallback(tableName, (value) => {
+      if (predicate(value)) {
+        rows.push(value);
       }
-    };
-  });
+    });
+    return rows;
+  }
 }
 
 type Query =
@@ -178,16 +181,7 @@ function keyRange(query?: Query) {
   }
 }
 
-/** custom  */
-export async function dbfilter<T extends keyof DB>(
-  tableName: T,
-  predicate: (value: DB[T]) => boolean
-) {
-  const rows: Array<DB[T]> = [];
-  await dbopenCursorCallback(tableName, (value) => {
-    if (predicate(value)) {
-      rows.push(value);
-    }
-  });
-  return rows;
-}
+//const x = new Idb<{hello: {key:string, apa:string}}>()
+//x.add("hello",{key:"1",apa:""}).then(res=>{
+//  res
+//})
