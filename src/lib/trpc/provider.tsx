d72f13a27b728370dcf189e82ba6596464d9f4e5
andy <andersgee@gmail.com>
2024-12-10 19:23:24 +0100

    lint

--- src/lib/trpc/provider.tsx
+++ src/lib/trpc/provider.tsx
@@ -1,40 +1,6 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  keepPreviousData,
-} from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { idbapi } from "./hook";
-//import { debugLink } from "./debug-link";
-import { routerCallerLink } from "./router-caller-link";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      enabled: true,
-      staleTime: Infinity,
-      refetchInterval: false,
-      refetchIntervalInBackground: false,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      refetchOnMount: false,
-      retryOnMount: true,
-      throwOnError: false,
-      retry: false,
-      //placeholderData: (x) => x, // aka keepPreviousData
-      placeholderData: keepPreviousData,
-
-      //retry: 3,
-      //retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
-      //the default JSON.sringify will error on anything without .toJSON() method. like bigint or typed arrays etc
-      //queryKeyHashFn: (k) => JSONE.stringify(k),
-    },
-  },
-});
-
-export const trpcClient = idbapi.createClient({
-  //links: [debugLink, routerCallerLink()],
-  links: [routerCallerLink()],
-});
+import { queryClient, trpcClient } from "./trpc-client";
 
 export function TrpcProvider({ children }: { children: React.ReactNode }) {
   return (
---
+++ src/lib/trpc/trpc-client.ts
@@ -0,0 +1,33 @@
+import { QueryClient, keepPreviousData } from "@tanstack/react-query";
+import { idbapi } from "./hook";
+//import { debugLink } from "./debug-link";
+import { routerCallerLink } from "./router-caller-link";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      enabled: true,
+      staleTime: Infinity,
+      refetchInterval: false,
+      refetchIntervalInBackground: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      retryOnMount: true,
+      throwOnError: false,
+      retry: false,
+      //placeholderData: (x) => x, // aka keepPreviousData
+      placeholderData: keepPreviousData,
+
+      //retry: 3,
+      //retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+      //the default JSON.sringify will error on anything without .toJSON() method. like bigint or typed arrays etc
+      //queryKeyHashFn: (k) => JSONE.stringify(k),
+    },
+  },
+});
+
+export const trpcClient = idbapi.createClient({
+  //links: [debugLink, routerCallerLink()],
+  links: [routerCallerLink()],
+});
